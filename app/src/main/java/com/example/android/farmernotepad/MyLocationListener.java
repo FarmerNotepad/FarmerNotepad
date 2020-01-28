3430920ab3b80e3455e21649d7d8bf1c80d4023f
nickspo <44384322+nickspo@users.noreply.github.com>
2020-01-28 18:29:02 +0200

    Location#1

---
+++ app/src/main/java/com/example/android/farmernotepad/MyLocationListener.java
@@ -0,0 +1,27 @@
+package com.example.android.farmernotepad;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+public class MyLocationListener implements LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
