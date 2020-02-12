9adea9aca57281b6664f63eeae372b5f415bd018
nickspo <44384322+nickspo@users.noreply.github.com>
2020-02-12 19:26:16 +0200

    Export

--- app/src/main/java/com/example/android/farmernotepad/Backup.java
+++ app/src/main/java/com/example/android/farmernotepad/Backup.java
@@ -3,10 +3,12 @@ package com.example.android.farmernotepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Backup extends AppCompatActivity {
 
@@ -22,11 +24,17 @@ public class Backup extends AppCompatActivity {
         final CheckBox checkOnline = findViewById(R.id.checkBoxOnline);
         final TextView localPath = findViewById(R.id.textViewPath);
 
+        btnExport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseHelper dbHelper = new DatabaseHelper(Backup.this);
+                dbHelper.exportDB(Backup.this);
+                Toast.makeText(Backup.this,"Database exported to " + Backup.this.getExternalFilesDir(null),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-    public void prepareDB(){
-            //TO DO Copy db file and prepare it to be exported/sent
-    }
 
     public void mailDB(){
             //TO DO send email with database attached
