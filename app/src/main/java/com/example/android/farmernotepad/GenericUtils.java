d1b07f7e9bd4e9e531b2335c1f91071ac08931b6
nickspo <44384322+nickspo@users.noreply.github.com>
2020-02-18 20:37:53 +0200

    ImportStuff

--- app/src/main/java/com/example/android/farmernotepad/GenericUtils.java
+++ app/src/main/java/com/example/android/farmernotepad/GenericUtils.java
@@ -111,26 +111,6 @@ public class GenericUtils {
         return myList;
     }
 
-    public static void copyFile(FileInputStream fromFile, FileOutputStream toFile) throws IOException {
-        FileChannel fromChannel = null;
-        FileChannel toChannel = null;
-        try {
-            fromChannel = fromFile.getChannel();
-            toChannel = toFile.getChannel();
-            fromChannel.transferTo(0, fromChannel.size(), toChannel);
-        } finally {
-            try {
-                if (fromChannel != null) {
-                    fromChannel.close();
-                }
-            } finally {
-                if (toChannel != null) {
-                    toChannel.close();
-                }
-            }
-        }
-    }
-
 
     public static boolean isOnline() {
         try {
