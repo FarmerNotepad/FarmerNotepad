c0609efe203448c4ca7b92af677d806a29097240
PeterGogis <32797410+PeterGogis@users.noreply.github.com>
2020-03-10 19:37:35 +0200

    Set ImageButtons to Tabs by id and onClickListeners

--- app/src/main/java/com/example/android/farmernotepad/FragmentView.java
+++ app/src/main/java/com/example/android/farmernotepad/FragmentView.java
@@ -7,58 +7,52 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentView#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentView extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ImageButton list, grid, staggered;
 
     public FragmentView() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentView.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentView newInstance(String param1, String param2) {
-        FragmentView fragment = new FragmentView();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_view, container, false);
+
+        list = view.findViewById(R.id.listBtn);
+        grid = view.findViewById(R.id.gridBtn);
+        staggered = view.findViewById(R.id.staggeredBtn);
+
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        grid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        staggered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        return view;
     }
 }
