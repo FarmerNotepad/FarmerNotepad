c709c1d72eceab5affab0b6d38d19fd8722cbe0b
nickspo <44384322+nickspo@users.noreply.github.com>
2020-03-11 17:23:50 +0200

    filterColor

--- app/src/main/java/com/example/android/farmernotepad/FragmentColor.java
+++ app/src/main/java/com/example/android/farmernotepad/FragmentColor.java
@@ -1,16 +1,20 @@
 package com.example.android.farmernotepad;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 
 public class FragmentColor extends Fragment {
-
+    MainActivity myActivity;
     private ImageButton allColors, redBtn, orangeBtn, yellowBtn, greenBtn, blueBtn, purpleBtn, blackBtn, greyBtn, whiteBtn;
 
 
@@ -19,12 +23,20 @@ public class FragmentColor extends Fragment {
     }
 
 
+    private Context context;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_color, container, false);
-
+        myActivity = (MainActivity) getActivity();
         allColors = view.findViewById(R.id.allColorsBtn);
         redBtn = view.findViewById(R.id.redBtn);
         orangeBtn = view.findViewById(R.id.orangeBtn);
@@ -37,15 +49,15 @@ public class FragmentColor extends Fragment {
         whiteBtn = view.findViewById(R.id.whiteBtn);
 
         setClickListener(allColors, 0);
-        setClickListener(redBtn, 1);
-        setClickListener(orangeBtn, 2);
-        setClickListener(yellowBtn, 3);
-        setClickListener(greenBtn, 4);
-        setClickListener(blueBtn, 5);
-        setClickListener(purpleBtn, 6);
-        setClickListener(blackBtn, 7);
-        setClickListener(greyBtn, 8);
-        setClickListener(whiteBtn, 9);
+        setClickListener(redBtn, ContextCompat.getColor(context,R.color.Red));
+        setClickListener(orangeBtn, ContextCompat.getColor(context,R.color.Orange));
+        setClickListener(yellowBtn, ContextCompat.getColor(context,R.color.Yellow));
+        setClickListener(greenBtn, ContextCompat.getColor(context,R.color.Green));
+        setClickListener(blueBtn, ContextCompat.getColor(context,R.color.Blue));
+        setClickListener(purpleBtn, ContextCompat.getColor(context,R.color.Purple));
+        setClickListener(blackBtn, ContextCompat.getColor(context,R.color.Black));
+        setClickListener(greyBtn, ContextCompat.getColor(context,R.color.Grey));
+        setClickListener(whiteBtn, ContextCompat.getColor(context,R.color.White));
 
 
         return view;
@@ -55,7 +67,12 @@ public class FragmentColor extends Fragment {
         colorBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                GenericUtils.toast(getContext(), "Clicked");
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(myActivity.getApplicationContext());
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("filter_color", colorNumber);
+                editor.apply();
+                myActivity.filterColor(colorNumber);
             }
         });
     }
